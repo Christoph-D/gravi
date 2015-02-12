2bd734daef273259d8457e1b01a1fd51a295809b
Christoph Dittmann <github@christoph-d.de>
2015-02-12 11:31:52 +0100

    Add jasmine testing framework

--- build.sh
+++ build.sh
@@ -7,20 +7,24 @@ else
 fi
 
 compile() {
+  src=$1
+  out=$2
+  coffee=${out%.js}.coffee
   # Ignore the output because coffeescript-concat sometimes fails when
   # it tries to concat temporary hidden files created by an editor.
-  ./coffeescript-concat/coffeescript-concat -I coffee -o js/gralog.coffee 2>/dev/null
+  ./coffeescript-concat/coffeescript-concat -I "$src" -o "$coffee" #2>/dev/null
   # Recompile to javascript
-  coffee -co js/ js/gralog.coffee
-  rm -f js/gralog.coffee
+  coffee -bco "${out%/*}" "$coffee"
+  rm -f "$coffee"
 }
 
 while :; do
   echo -n 'Recompiling...' | ts
-  compile
+  compile coffee js/gralog.js
+  compile spec jasmine/spec/gralog.js
   echo 'done'
   if which inotifywait >/dev/null; then
-    inotifywait -qqre modify,create,delete,move --exclude '/\.[^/]*' coffee/
+    inotifywait -qqre modify,create,delete,move --exclude '/\.[^/]*' coffee spec
   else
     sleep 1
   fi
