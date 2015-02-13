acaae05edc6189310007dc7f317c623ccdb9e916
Christoph Dittmann <github@christoph-d.de>
2015-02-13 18:25:44 +0100

    Source maps

--- build.sh
+++ build.sh
@@ -12,14 +12,12 @@ compile() {
   # it tries to concat temporary hidden files created by an editor.
   ./coffeescript-concat/coffeescript-concat -I coffee -o "$coffee" 2>/dev/null
   # Recompile to javascript
-  coffee -bco js/ "$coffee"
-  rm -f "$coffee"
+  coffee -mbco js/ "$coffee"
 }
 compiletests() {
   coffee=jasmine/spec/gralog.coffee
   cat spec/*.coffee > "$coffee"
-  coffee -bco jasmine/spec/ "$coffee"
-  rm -f "$coffee"
+  coffee -mbco jasmine/spec/ "$coffee"
 }
 
 while :; do
