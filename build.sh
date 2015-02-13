8dc20373914a2aebd94b385055de9348ccdcb50c
Christoph Dittmann <github@christoph-d.de>
2015-02-13 23:21:52 +0100

    More robust build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+if ! coffee --help &>/dev/null; then
+  echo 'Please install coffee-script:'
+  echo '$ npm install coffee-script'
+  echo 'And possibly add the following line to ~/.profile or ~/.bashrc or similar:'
+  echo 'export PATH="$HOME/node_modules/.bin:$PATH"'
+  exit 1
+fi
+
+mapping='-m'
+if ! coffee -m --help &>/dev/null; then
+  echo 'Your coffee-script version is:'
+  coffee --version
+  echo 'This seems to be older than 1.6.'
+  echo 'Please upgrade coffee-script in order to use source maps.'
+  echo
+  mapping=''
+fi
+
 if which inotifywait >/dev/null; then
   echo 'Watching the source directory for changes.'
 else
-  echo 'Recompiling every 1.0s because inotify-tools for efficient watching is unavailable.'
+  echo -n 'Cannot find inotifywait.'
+  if [[ $(lsb_release -is 2>/dev/null) = Ubuntu ]]; then
+    echo ' You can fix this with:'
+    echo 'sudo apt-get install inotify-tools'
+  else
+    echo
+  fi
+  echo
+  echo 'Recompiling every 1.0s because inotifywait for efficient watching is unavailable.'
 fi
 
 compile() {
@@ -12,12 +38,12 @@ compile() {
   # it tries to concat temporary hidden files created by an editor.
   ./coffeescript-concat/coffeescript-concat -I coffee -o "$coffee" 2>/dev/null
   # Recompile to javascript
-  coffee -mbco js/ "$coffee"
+  coffee $mapping -bco js/ "$coffee"
 }
 compiletests() {
   coffee=jasmine/spec/gralog.coffee
   cat spec/*.coffee > "$coffee"
-  coffee -mbco jasmine/spec/ "$coffee"
+  coffee $mapping -bco jasmine/spec/ "$coffee"
 }
 
 while :; do
