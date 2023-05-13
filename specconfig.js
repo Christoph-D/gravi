6ffb033946139867ab8474a10438ef24effba509
Christoph Dittmann <github@yozora.eu>
2023-05-13 17:28:33 +0200

    Remove package-lock.json and specconfig.js
    These files are unnecessary.

--- specconfig.js
+++
@@ -1,7 +0,0 @@
-/*eslint-env browser, commonjs*/
-/*eslint no-var: 0*/
-
-require('core-js/stable');
-
-const testsContext = require.context("./spec", false, /\.js$/);
-testsContext.keys().forEach(testsContext);
